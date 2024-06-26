dd19226f2bcea6fb23a03e8d799f4efd2ba8099d
Andrea Churchwell <achurchwell1.prsvr@gmail.com>
2024-06-26 13:16:18 -0500

    finished while loops

--- JS/assignments/week05/while-e-coyote/main.js
+++ JS/assignments/week05/while-e-coyote/main.js
@@ -1,15 +1,46 @@
 function numberDoubler(num) {
+
     // Your code here
+    let count = 0
+    while (num <= 100) {
+        //  num *= 2
+        num = num * 2
+    }
+
+    return num
+
+
 }
 
+
+// console.log(numberDoubler(99))
+
 function stringRepeater(str) {
+
     // Your code here
+    str2 = "";
+    while (str2.length < 10) {
+        str2 = str2 + str
+    }
+    return str2
+
+
 }
 
 function makeDivisible(x, y) {
-    // Your code here
-}
 
+    // Your code here
+
+    while (x % y != 0) {
+        x++
+        // console.log("x",x, "y",y)
+    }
+
+
+    return x
+
+}
+// console.log(makeDivisible(2, 10))
 module.exports = {
     numberDoubler,
     stringRepeater,
