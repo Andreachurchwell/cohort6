28d5a4646cd5d919cf458c195d189d2058f85976
Andrea Churchwell <achurchwell1.prsvr@gmail.com>
2024-07-03 14:27:00 -0500

    finished extra problems

--- JS/assignments/week05/extra-problems/main.js
+++ JS/assignments/week05/extra-problems/main.js
@@ -1,20 +1,28 @@
+
 function isEvenlyDivisible(num1, num2) {
+
   if (num1 % num2 == 0) {
     return true
   } else {
     return false
   }
 
-
-
 }
+
+
+
 
 function halfSquare(num) {
-  if (num * num / 2 === num) {
-    return num
 
-  }
+  const squared = num * num;
+  const half = squared / 2;
+  return half
+
 }
+
+
+
+
 
 function isLong(str) {
 
@@ -26,53 +34,152 @@ function isLong(str) {
 
 }
 
-function exclaim(str) {
-  if (str.length - 1 == '!') {
-    return str
-  } else {
-    return str + '!'
-  }
 
+
+
+
+function exclaim(str) {
+
+  let cleanStr = str.replace(/!+$/, '');
+  return cleanStr + '!'
+}
+
+
+
+
+function containsDigit(str) {
+  const digitRegex = /\d/;
+  if (str.length === 0) {
+    return false;
+  }
+  return digitRegex.test(str)
+}
+
+
+
+function containsLowerCase(str) {
+
+  const lowerRegex = /[a-z]/;
+  if (str.length === 0) {
+    return false
+  }
+  return lowerRegex.test(str)
+}
+
+
+
+
+function containsUpperCase(str) {
+  const upperRegex = /[A-Z]/;
+  if (str.length === 0) {
+    return false
+  }
+  return upperRegex.test(str)
+}
+
+
+
+
+function containsNonAlphanumeric(str) {
+  const nonAlphaRegex = /[^a-zA-Z0-9]/;
+  if (str.length === 0) {
+    return false
+  }
+  return nonAlphaRegex.test(str)
 
 
 }
+
+
+
+
+function containsSpace(str) {
+  const spacesRegex = /\s/;
+  if (str.length === 0) {
+    return false
+  }
+  return spacesRegex.test(str)
+
+
+}
+
+
+
+function digits(num) {
+
+  const numString = num.toString().replace(/[^0-9]/g, '');
+  return Array.from(numString).map(Number)
+
+
+}
+
+
+
+
+function truncate(str) {
+
+  if (str.length >= 15) {
+    return str.slice(0, 8) + '...'
+  } else {
+    return str
+  }
+
+
+}
+
+
+
+
+
+function isValidPassword(password) {
+ 
+
+  if(/\s/.test(password)) return false;
+  if(!/[A-Z]/.test(password)) return false;
+  if(!/[a-z]/.test(password)) return false;
+  if (!/\d/.test(password)) return false;
+  if (!/\W/.test(password)) return false;
+  return true;
+
+}
+
+
+
+
+function onlyPunchy(words) {
+  return words.map(word => {
+    let cleanword = word.replace(/!+/g, '!');
+    if (!cleanword.includes('!')) {
+      cleanword += '!';
+
+    }
+    let wordWithout = cleanword.replace(/!/g, '!');
+    if (wordWithout.length < 15) {
+      return cleanword
+    }
+  }).filter(Boolean);
+
+}
+
+
 
 function countWords(str) {
 
-}
+  str = str.trim();
 
-function containsDigit() {
-}
-
-function containsLowerCase() {
-}
-
-function containsUpperCase() {
-}
-
-function containsNonAlphanumeric() {
-}
-
-function containsSpace() {
-}
-
-function digits() {
-}
-
-function truncate() {
-}
-
-function isValidPassword(str) {
-  if (str === str) {
-    return true
-  } else {
-    return false
+  if (str.length === 0) {
+    return 0
   }
+  const splitStr = str.split(/\s+/);
+  return splitStr.length
+
 }
 
-function onlyPunchy(arr) {
 
-}
+
+
+
+
 
 
 module.exports = {
